be477437273194626130b328f07086fcefab4f90
Leirannt <93717185+Leirannt@users.noreply.github.com>
2023-12-23 18:05:03 +0700

    aaa
    commit2

--- resources/views/Registration.blade.php
+++ resources/views/Registration.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TeluMart</title>
-    <!-- <link rel="stylesheet" href="{{ asset('view.css') }}"> -->
+    <link rel="stylesheet" href="{{ asset('view.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
@@ -13,9 +13,9 @@
     <div class="wrapp">
         <div class="container main">
             <div class="row">
-                <!-- <div class="col-md-6 side-image">
-                    <img src="logo2.png" alt="image">
-                </div> -->
+                <div class="col-md-6 side-image">
+                    <img src="" alt="">
+                </div>
                 <form action="{{route('regist-user')}}" method="post">
                     @if(Session::has('success'))
                     <div class="alert alert-success">{{Session::get('success')}}</div>
@@ -30,17 +30,17 @@
                             <div class="tempatRegistrasi">
                                 <header>Create Account</header>
                                 <div class="input-field">
-                                    <input type="text" class="input" name="Username" value="{{old('Username')}}" required>
+                                    <input type="text" class="input" name="username" required autocomplete="off">
                                     <label for="username">Username</label>
                                     <span class="text-danger">@error('Username') {{$message}} @enderror</span>
                                 </div>
                                 <div class="input-field">
-                                    <input type="text" class="input" name="Email" value="{{old('Email')}}" required autocomplete="off">
+                                    <input type="text" class="input" name="email" required autocomplete="off">
                                     <label for="email">Email</label>
                                     <span class="text-danger">@error('Email') {{$message}} @enderror</span>
                                 </div>
                                 <div class="input-field">
-                                    <input type="password" class="input" name="Password" value="{{old('Password')}}" required>
+                                    <input type="password" class="input" name="password" required autocomplete="off">
                                     <label for="password">Password</label>
                                     <span class="text-danger">@error('Password') {{$message}} @enderror</span>
                                 </div>
